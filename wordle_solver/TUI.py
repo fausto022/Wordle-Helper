27caefe0c7e22d8fa79852a256f00085c2a06a69
fausto022 <faustobotto@hotmail.com>
2022-01-04 00:45:55 -0300

    Reformatting continues. wordle_solver.py in progress.

--- wordle_solver/TUI.py
+++ wordle_solver/TUI.py
@@ -1,7 +1,5 @@
 """Handles the prompting on the terminal, as well as collecting the user's data about their game"""
 
-import string
-
 def char_from_user(prompt: str) -> chr:
     """Ask the user for a character for an alphabetic character, can be empty."""
     user_input = input(prompt).lower()
@@ -17,25 +15,26 @@ def list_of_chars_from_user(prompt: str) -> list[chr]:
         return []
     elif not user_input.isalpha():
         list_of_chars_from_user(prompt)
-
     return [char for char in user_input]
 
-def dict_of_greens_from_user(prompt: str) -> dict[int: chr]:
-    """Prompts the user for their green letters, in the form ' d _ c _ _ ' or 'd_c__' """
+def guess_from_user(prompt: str) -> str:
+    """Prompts the user for their green or yellow letters, 
+    in the form ' d _ c _ _ ' or 'd_c__' 
+    """
     user_input = input(prompt)    
     user_input = user_input.replace(' ', '')
 
     #ensure correct usage
-    valid_input = True
+    valid_input = len(user_input == 5)
     for c in user_input:
         if c != '_' or not c.isAlpha():
-            valid_input = False
-    if not (valid_input and len(user_input) == 5):
+            valid_input = False    
+    if not valid_input:
             dict_of_greens_from_user(prompt)
 
     #return the data in the appropiate format
-    return_dict = {0:'', 1:'', 2:'', 3:'', 4:''}
-    for k, letra in enumerate(user_input):
-        if letra.isalpha():
-            return_dict[k] = letra
+    return_dict = {0: '', 1: '', 2: '', 3: '', 4: ''}
+    for k, letter in enumerate(user_input):
+        if letter != '_':
+            return_dict[k] = letter
     return return_dict
--- wordle_solver/wordle_solver.py
+++ wordle_solver/wordle_solver.py
@@ -1,24 +1,50 @@
-def cumple_requisitos(palabra, verdes, amarillas, descartadas) -> bool:
-    """Se fija si determinada palabra cumple con las letras verdes,
-    faltan descartadas y amarillas. (todo)"""    
+from wordle_solver import english_dictionary as e_d
 
-    # Comparamos las verdes
-    for k, v in verdes.items():
-        if v != '' and palabra[k] != v:
-            return False   
+class wordle_solver():
+    """Provides the methods to find the optimal word, and a list of all the possible words"""
+    def __init__(self, path: str=''): #TODO: add path functionality
+        self._e_d = e_d.english_dictionary()
+        self._greens = {1: '', 2: '', 3: '', 4: '', 5: ''}
+        self._yellows = {1: [], 2: [], 3: [], 4: [], 5: []}
+        self._discarded = []
+        
+    def add_guess(greens: str, yellows: str, grays: str) -> None:
+        """Process the information coming from this new guess. This function also applies different
+        heuristics/inferences I came up with to further narrow the information provided by each guess.        
+        """
+        #TODO: Implement heuristics/inferences
+        #TODO: Explain said heuristics/inferences in the README
+        for k, l in enumerate(greens):
+            if l != '_':self._greens[k] = l
+        for k, l in enumerate(yellows):
+            if l != '_':self._yellows[k].append(l)
+        self._discarded = self._discarded + list(grays)
+
+    def is_correct(word) -> bool:
+        """Given a word, checks if it could be a possible correct answer """    
+
+        # Comparamos las verdes
+        for k, v in verdes.items():
+            if v != '' and palabra[k] != v:
+                return False   
     
-    # Tiene que contener las amarillas pero no en el lugar que van
-    for k, v in amarillas.items():
-        for letra in v:
-            if not(letra in palabra and palabra[k] != letra):
+        # Tiene que contener las amarillas pero no en el lugar que van
+        for k, v in amarillas.items():
+            for letra in v:
+                if not(letra in palabra and palabra[k] != letra):
+                    return False
+    
+        # Falso si contiene una letra descartada
+        for letra in descartadas:
+            if letra in palabra and letra not in verdes.values():
                 return False
-    
-    # Falso si contiene una letra descartada        
-    for letra in descartadas:
-        if letra in palabra and letra not in verdes.values():
-            return False
 
-    return True       
+        return True       
+
+
+
+
+
  
 
 def filtrar_palabras(lista: list[str], verdes: dict[int, chr], amarillas: dict[int, list], descartadas: list[chr]) -> list[str]:
@@ -31,7 +57,8 @@ def filtrar_palabras(lista: list[str], verdes: dict[int, chr], amarillas: dict[i
 def elegir_palabra(lista: list[str], verdes: dict[int, chr], restantes: list[chr]) -> str:
     """Dada una lista de palabras posibles, devuelve la palabra mas óptima.
     Óptima siendo aquella palabra que descarta la mayor cantidad de palabras en la lista, fijándose que contenga las letras
-    que mas aparecen en la mayor cantidad de palabras en la lista."""
+    que mas aparecen en la mayor cantidad de palabras en la lista.
+    """
     print("Eligiendo palabra . . .")
     palabras_con_letra = dict.fromkeys(restantes, set()) #{i: ("incas", "arids")}
     print("Recorriendo palabras de la lista . . .")
@@ -59,22 +86,11 @@ def elegir_palabra(lista: list[str], verdes: dict[int, chr], restantes: list[chr
 
 
 def main():
-    with open("words_5.txt", "r") as f:
-        lista_palabras_1 = f.read().split("\n") # todas las palabras de 5 letras del idioma inglés
-    
-    with open("words_5_2.txt", "r") as f:
-        lista_palabras_2 = f.read().split(' ')
 
-    alfabeto = list(string.ascii_lowercase) # las 26 letras del alfabeto inglés, en una lista.   
 
 
     verdes = dict_de_verdes_usuario("Letras en verde (en formato 'a _ b c _) -> ")
     print(f"verdes: {verdes}")
-
-    amarillas = {0:[], 1:[], 2:[], 3:[], 4:[]}           
-    for k in amarillas:
-        amarillas[k] = lista_de_chars_usuario(f"¿Cuáles tenés amarillas en {k+1}?: ")
-    print(f"amarillas: {amarillas}")    
 
     # pedirle al usuario las letras que ya descartó (grises)
     descartadas = lista_de_chars_usuario("Letras descartadas (en formato 'abc') -> ") 
