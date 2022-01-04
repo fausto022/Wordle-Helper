66bd36035d75445b34ab68c5dd555c271f569936
fausto022 <faustobotto@hotmail.com>
2022-01-04 18:19:00 -0300

    wordle_solver basic functionality implemented. TODO: TUI and main.

--- wordle_test.py
+++ wordle_test.py
@@ -1,106 +1,55 @@
 from wordle_solver import english_dictionary as e_d
+from wordle_solver import lists
+from enum import Enum
+
+
 
 class wordle_solver():
-    """Provides the methods to find the optimal word, and a list of all the possible words"""
-    def __init__(self, path: str=''): #TODO: add path functionality
+    """Provides the methods to find the optimal word, and a list of all the possible words."""
+
+    def __init__(self, path: str=''): #TODO: add path functionality.
         self._e_d = e_d.english_dictionary()
         self._greens = {1: '', 2: '', 3: '', 4: '', 5: ''}
         self._yellows = {1: [], 2: [], 3: [], 4: [], 5: []}
         self._discarded = []
+        self._valid_words = self._e_d.words_list
+
+    def is_valid(self, word) -> bool:
+        """Given a word, checks if it could be a possible correct answer."""
+        valid_word = True
+        for i, l in enumerate(word):
+            # for the word to be valid, all letters have to be valid.
+            if self._greens[i] != '':
+                valid_letter = (l == self._greens[i])
+            else:
+                valid_letter = l not in self._yellows[i] and l not in self._discarded
+            valid_word = valid_word and valid_letter 
+        return valid_word
         
-    def add_guess(greens: str, yellows: str, grays: str) -> None:
+    def add_guess(self, greens: str, yellows: str, grays: str) -> None:
         """Process the information coming from this new guess. This function also applies different
-        heuristics/inferences I came up with to further narrow the information provided by each guess.        
+        heuristics/inferences I came up with to further narrow the information provided by each guess.
+        Automatically updates the list of valid words.
         """
-        #TODO: Implement heuristics/inferences
-        #TODO: Explain said heuristics/inferences in the README
+        #TODO: Implement heuristics/inferences.
+        #TODO: Explain said heuristics/inferences in the README.
         for k, l in enumerate(greens):
             if l != '_':self._greens[k] = l
         for k, l in enumerate(yellows):
             if l != '_':self._yellows[k].append(l)
         self._discarded = self._discarded + list(grays)
+        self._filter_words()
 
-    def is_correct(word) -> bool:
-        """Given a word, checks if it could be a possible correct answer """    
+    def _filter_words(self) -> None:
+        """Update the list of all valid words with the current information."""
+        self._valid_words = [word for word in self._valid_words if self.is_valid(word)]
 
-        # Comparamos las verdes
-        for k, v in verdes.items():
-            if v != '' and palabra[k] != v:
-                return False   
-    
-        # Tiene que contener las amarillas pero no en el lugar que van
-        for k, v in amarillas.items():
-            for letra in v:
-                if not(letra in palabra and palabra[k] != letra):
-                    return False
-    
-        # Falso si contiene una letra descartada
-        for letra in descartadas:
-            if letra in palabra and letra not in verdes.values():
-                return False
-
-        return True       
-
-
-
-
-
- 
-
-def filtrar_palabras(lista: list[str], verdes: dict[int, chr], amarillas: dict[int, list], descartadas: list[chr]) -> list[str]:
-    """Filtra las palabras según cumplan con los requisitos"""
-    print("Filtrando palabras . . .")
-    lista = [palabra for palabra in lista if cumple_requisitos(palabra, verdes, amarillas, descartadas)]
-    print("Palabras han sido filtradas . . .")
-    return lista
-
-def elegir_palabra(lista: list[str], verdes: dict[int, chr], restantes: list[chr]) -> str:
-    """Dada una lista de palabras posibles, devuelve la palabra mas óptima.
-    Óptima siendo aquella palabra que descarta la mayor cantidad de palabras en la lista, fijándose que contenga las letras
-    que mas aparecen en la mayor cantidad de palabras en la lista.
-    """
-    print("Eligiendo palabra . . .")
-    palabras_con_letra = dict.fromkeys(restantes, set()) #{i: ("incas", "arids")}
-    print("Recorriendo palabras de la lista . . .")
-    for palabra in lista:        
-        for i, letra in enumerate(palabra):
-            if letra != verdes[i]:
-                palabras_con_letra[letra] = palabras_con_letra[letra] | {palabra}
-
-    print("Viendo cuantas palabras descarta cada palabra . . .")
-    cuales_descarta_palabra = dict.fromkeys(lista, set())
-    for palabra in lista:        
-        for letra in palabra:
-            cuales_descarta_palabra[palabra] = cuales_descarta_palabra[palabra] | palabras_con_letra[letra]
-    
-    print(f"cuantas palabras quedan: {len(lista)}")
-    if len(lista) < 20:
-        print(lista)
-
-    max = lista[0]
-    for palabra in lista:
-        if len(cuales_descarta_palabra[palabra]) > len(cuales_descarta_palabra[max]):
-            max = palabra
-    return max
-
-
-
-def main():
-
-
-
-    verdes = dict_de_verdes_usuario("Letras en verde (en formato 'a _ b c _) -> ")
-    print(f"verdes: {verdes}")
-
-    # pedirle al usuario las letras que ya descartó (grises)
-    descartadas = lista_de_chars_usuario("Letras descartadas (en formato 'abc') -> ") 
-    print(f"descartadas: {descartadas}")
-    descartadas = [letra for letra in descartadas if letra not in verdes.values()]
-
-    # nos quedamos solo con las letras que no descartamos
-    restantes = [letra for letra in alfabeto if letra not in descartadas] 
-    
-
-    lista_final = (filtrar_palabras(lista_palabras_2, verdes, amarillas, descartadas))
-    resultado = elegir_palabra(lista_final, verdes, restantes)
-    print(f"La mejor palabra es: {resultado}")  
+    def choose_word(self, valid_words: bool = None) -> str:
+        """ 
+        """
+        # TODO: rethink the working of this function.
+        # For if you want to print BOTH best words (the valid and the non valid) you would have to run this function TWICE, which is not optimal.
+        if valid_words:valid_words = self._valid_words
+        words_x_letter = self._e_d.words_by_letter(valid_words)
+        best_word = max(words_x_letter.keys(), key=lambda x: words_x_letter[x])
+        return best_word
