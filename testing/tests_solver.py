997c69c8ebe2d45262ea049a402d985ed5a5b1c1
fausto022 <faustobotto@hotmail.com>
2022-01-05 23:43:04 -0300

    :(

--- testing/tests_solver.py
+++ testing/tests_solver.py
@@ -1,12 +1,5 @@
 import unittest
 from wordle_solver.solver import solver
 
-class word_choosing(unittest.TestCase):
-    def setUp(self):
-        self.main_solver = solver("5_letter_words_ver.2.txt")
-
-    def test_choose_word_1(self):
-        self.assertEqual(self.main_solver.choose_word(), )
-
 if __name__ == '__main__':
     unittest.main()
