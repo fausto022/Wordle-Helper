e5f57695e2af690618b90d3472f5987e8101facb
fausto022 <faustobotto@hotmail.com>
2022-01-14 00:45:49 -0300

    TUI almost finished.

--- wordle_solver/TUI.py
+++ wordle_solver/TUI.py
@@ -1,71 +1,227 @@
 """Handles the prompting on the terminal, collects the user's data about their game, and runs the main loop of the program"""
 
-from typing import Match
-from wordle_solver import solver
+from wordle_solver.solver import solver #it's the only class in the module, so I'm importing it directly :)
+import curses
 
-def run_guessing_loop() -> int:
-    """Runs the main loop for your wordle_helper"""
-    options = """1)Show me the word with maximum coverage for the current info.
-2)Show me the best guess with maximum coverage for the current info.
-3)Show me a list of the possible guesses.
-4)Keep guessing.
-?)Help.
-"""
-    difference = """The difference between options 1) and 2), is that the first option will try to affect as many remaining words as possible without restricting itself to possible guesses, since the word that minimizes the scope COULD contain a discarded letter.
-    The second option will give you the word that reduces the scope as much as possible while matching your greens, yellows and grays, meaning that if you are aim is to get it right in the next guess, and don't care for how many words your guess will affect, this is the word you should choose.
-    Note: Testing has proven this heuristic to not always be right, and sometimes using option 1) will leave you with more possible guesses than option 2), making option 2) a strictly better guess."""
+#renamings
+(down, up) = (-1, 1)
+(left, right) = (-1, 1)
+(gray, yellow, green) = (0, 1, 2)
 
-    solver_instance = solver.solver("5_letter_words.txt")       
-    print(f"There are {len(solver_instance.valid_words)} possible words.")
+def isLetter(key: int) -> bool:
+    return (ord('A') <= key <= ord('Z') or ord('a') <= key <= ord('z'))
+
+class curses_message:
+    def __init__(self, window: 'curses._CursesWindow', y: int, x: int, message: str = '') -> None:
+        self._pWindow = window #assign parent window
+        self._message = list(message) #as a list because we need to modify it in the future
+        self._y = y            #assign the position in the parent window
+        self._x = x            #assign the position in the parent window
+
+    def translate_to(self, y: int, x: int)-> None:
+        """Moves the guess to another point on the window."""
+        self._y = y
+        self._x = x
+
+    def draw_message(self) -> None:
+        """Displays the message in its current position"""
+        #This function won't be used very much but it's still nice to have, even if for debugging purposes
+        self._pWindow.addstr(self._message)
+
+class guess(curses_message):    
+    def __init__(self, window: 'curses._CursesWindow', y: int, x: int) -> None:
+        super().__init__(window, y, x)
+        self.letters = [{'l': '_', 'c': 0} for _ in range(5)]
+        self.pos = 0
+
+    def redraw_letter(self, pos: int = None) -> None:
+        """Redraws the letter in the specified position at its corresponding screen position, redraws the currently highlighted letter if pos is empty"""
+        if pos == None:
+            pos = self.pos
+        self._pWindow.addch(self._y, (self._x + pos*2), self.pos_l(pos),  self.l_color(pos) | curses.A_UNDERLINE)
+        
+    def display_word(self) -> None:
+        """Displays the resulting word"""        
+        for i, _ in enumerate(self.letters):
+            self.redraw_letter(i)
+
+    def l_color(self, pos: int = None) -> int:
+        """Returns the color_pair of a specified position, returns the color_pair of the currently highlighted letter if pos is empty"""
+        if pos == None:
+            pos = self.pos
+        return curses.color_pair(self.letters[pos]['c'])
+    
+    def pos_l(self, pos: int = None) -> chr:
+        """Returns the letter in a specified position, returns the currently highlighted letter if pos is empty"""
+        if pos == None:
+            pos = self.pos
+        return self.letters[pos]['l']
+
+    def pos_coord_x(self) -> int:
+        """Returns the cursor screen row according to the current position"""
+        return (self._x + self.pos * 2)        
+
+    def next(self, dir: int, wrapping) -> None:
+        """Move the cursor to the next position"""
+        if wrapping:
+            self.pos = (self.pos + dir) % 5
+        else:
+            self.pos = max(0, min(self.pos + dir, 4))
+
+    def change_l_col(self, dir, pos: int = None) -> int:
+        """Change the color of the specified position, changes the color the currently highlited letter if pos is empty"""
+        if pos == None:
+            pos = self.pos
+        new_col = (self.letters[pos]['c'] + dir) % 3
+        self.letters[self.pos]['c'] = new_col
+        return new_col
+
+    def set_l_color(self, color: int, pos: int = None):
+        if pos == None:
+            pos = self.pos
+        c = -1
+        while c != color:
+            c = self.change_l_col(1, pos)
+
+    def change_l(self, l: chr, pos: int = None) -> None:
+        """Change the letter of the currently highlighted position"""
+        if pos == None:
+            pos = self.pos 
+        self.letters[self.pos]['l'] = l 
+
+    def translate_up(self, i: int = 1) -> None:
+        """Moves the guess one line closer to the top edge."""
+        for _ in range(i):
+            super().translate_to(self._y - 1, self._x)
+        
+
+class guessing_interface:    
+    def __init__(self, y: int = 0, x: int = 0) -> None:
+        #init the new window and basic members
+        self.length = 24
+        self.width = 27
+        self._y = int(self.length * 5/6)
+        self._x = int((self.width/2) - 5)
+        self._window = curses.newwin(24, 26, y, x)
+        self._guess = guess(self._window, self._y, self._x)
+        self._past_guesses = []
+
+        #Create an instance of our solver
+        self._solver = solver("5_letter_words.txt")
+
+        #set the properties        
+        self._window.box()
+
+        #draw the initial elements
+        self._blank_prompt()
+
+        #finally refresh the window
+        self._window.refresh()
+
+        #Create a couple windows to display information
+        self._exploratory_guess = curses.newwin(1, 24, 1, 1)
+        self._exploratory_guess.addstr("Exploratory: stoae.")
+        self._exploratory_guess.refresh()
+        self._valid_guess = curses.newwin(1, 24, 2, 1)
+        self._valid_guess.addstr("Correct: stoae.")
+        self._valid_guess.refresh()
+
+    def _blank_prompt(self) -> None: 
+        self._window.addstr(self._y-1, self._x, "#          ") #erases the old cursor
+        self._window.addstr(self._y+1, self._x, "#          ") #erases the old cursor
+        self._window.addstr(self._y, self._x, "_ _ _ _ _ ")
+        self._guess = guess(self._window, self._y, self._x)
+        self._window.refresh()
+
+    def move_cursor(self, dir: int = 0, wrapping: bool = True) -> None:
+        """Moves the cursor graphic while also modifying the necessary class members,
+        dir = 0 will repaint the cursor without moving it"""
+        #erase current cursor        
+        self._window.addch(self._y - 1, self._guess.pos_coord_x(), ' ')
+        self._window.addch(self._y + 1, self._guess.pos_coord_x(), ' ')        
+        #draw the new cursor
+        self._guess.next(dir, wrapping)
+        cursor_x = self._guess.pos_coord_x()
+        self._window.addch(self._y - 1, cursor_x, '#', self._guess.l_color())
+        self._window.addch(self._y + 1, cursor_x, '#', self._guess.l_color())
+
+    def change_color(self, dir) -> None:
+        #change the color
+        self._guess.change_l_col(dir)
+        #redraw the letter        
+        self._guess.redraw_letter()
+        self.move_cursor()
+
+    def _submit_guess(self) -> None:        
+        if not any('_' == t['l'] for t in self._guess.letters):
+            #Update the solver
+            self._solver.add_guess(self._guess.letters)
+
+            #display the new words
+            best_exploratory, best_valid = self._solver.choose_word()            
+            self._exploratory_guess.addstr(0, 0, "Exploratory: " + best_exploratory + '.')
+            self._valid_guess.addstr(0, 0, "Correct: " + best_valid + '.')
+            self._exploratory_guess.refresh()
+            self._valid_guess.refresh()
+
+            #Only create a new prompt if we are not at the last guess
+            if len(self._past_guesses) < 6:
+                self._past_guesses.append(self._guess)
+                for past_guess in self._past_guesses:
+                    past_guess.translate_up(2)
+                    past_guess.display_word()
+                    self._blank_prompt()
+            else:
+                self._window.addstr(self._y-1, self._x, "          ") #erases the old cursor
+                self._window.addstr(self._y+1, self._x, "          ") #erases the old cursor            
+
+    def manage_input(self, key: int) -> None:
+        if len(self._past_guesses) < 6:
+            if isLetter(key):
+                self._guess.change_l(chr(key).lower())
+                self._guess.redraw_letter()
+                self.move_cursor(right, False)
+            elif key == curses.KEY_RIGHT:
+                self.move_cursor(right)
+            elif key == curses.KEY_LEFT:
+                self.move_cursor(left)
+            elif key == curses.KEY_UP:
+                self.change_color(up)
+            elif key == curses.KEY_DOWN:
+                self.change_color(down)
+            elif key == 8 or key == curses.KEY_BACKSPACE: #had to hardcode my own backspace key because curses wasn't detecting correctly
+                self._guess.change_l('_')
+                self._guess.set_l_color(gray)
+                self._guess.redraw_letter()
+            elif key == 13 or key == curses.KEY_ENTER: #had to hardcode my own enter key because curses wasn't detecting correctly
+                self._submit_guess()                    
+            self._window.refresh()
+
+def main(screen: 'curses._CursesWindow'):
+    #curses settings
+    curses.initscr()
+    curses.start_color()
+    curses.resize_term(24, 26) #resize the terminal so it fits our windows nicely
+    curses.curs_set(0) #make cursor invisible
+    curses.raw() #disable special inputs like enter/return
+ 
+    #init colors
+    curses.init_color(21, 156, 3, 4)
+    curses.init_pair(green, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(yellow, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+
+    terminal_y, terminal_x = screen.getmaxyx()
+    guessing_window = guessing_interface()
+
+    #run the main loop
     while True:
-        input("Press enter when you are ready to guess.")        
-        user_greens = _guess_from_user("Input your green letters in d_c_s format: ")
-        user_yellows = _guess_from_user("Input your yellow letters in d_c_s format: ")
-        user_grays = _guess_from_user("And finally, input your grays in dcs format: ", enforce_format = False)
-        print("Processing your data...")
-        solver_instance.add_guess(user_greens, user_yellows, user_grays)
-        print(f"There are {len(solver_instance.valid_words)} possible words remaining.")        
-        print(options, end = '')
-        while True:
-            user_option = input("\n-> ")
-            match user_option:
-                case '1':
-                    print("Option 1 selected")
-                    print(f"The best word now is: {solver_instance.choose_word(True)}")
-                case '2':
-                    print("Option 2 selected")
-                    print(f"The best guess now is: {solver_instance.choose_word(False)}")
-                case '3':
-                    if len(solver_instance.valid_words) > 49:
-                        print("There are too many possible gueses, showing you the first 50")
-                        print(solver_instance.valid_words[0:50])
-                    else:
-                        print(solver_instance.valid_words)
-                case '4':
-                    break
-                case '?':
-                    print(difference)
-                case _:
-                    print("Try again...")
-    return 0
+        key = screen.getch()
+        if key == 3: #if ctrl+c is pressed, quit.
+            break
+        else:
+            #screen.addch(int(terminal_y/2), int(terminal_x/2), key)
+            guessing_window.manage_input(key)  
 
-def _guess_from_user(prompt: str, enforce_format: bool = True) -> str:
-    """Prompts the user for their green or yellow letters, 
-    in the form ' d _ c _ _ ' or 'd_c__' 
-    """
-    user_input = input(prompt)    
-    user_input = user_input.replace(' ', '')
+    curses.endwin()
 
-    #ensure correct usage
-    if enforce_format:
-        valid_input = (len(user_input) == 5)
-        for c in user_input:
-            if c != '_' and (not c.isalpha()):
-                valid_input = False
-        if not valid_input:
-            _guess_from_user(prompt)
-    else:
-        for c in user_input:
-            if not c.isalpha():
-                valid_input = False
-    return user_input
+curses.wrapper(main)
--- wordle_solver/solver.py
+++ wordle_solver/solver.py
@@ -2,7 +2,7 @@ from wordle_solver import english_dictionary as e_d
 from enum import Enum
 
 
-
+(gray, yellow, green) = (0, 1, 2)
 class solver():
     """Provides the methods to find the optimal word, and a list of all the possible words."""
     def __init__(self, dictionary_file: str):
@@ -22,26 +22,27 @@ class solver():
                 valid_letter = (l == self._greens[i])
             else:
                 valid_letter = l not in self._yellows[i] and l not in self._discarded
-            valid_word = valid_word and valid_letter
-        
+            valid_word = valid_word and valid_letter        
         if any(l not in word for l in self._all_yellows): valid_word = False
         
         return valid_word
         
-    def add_guess(self, greens: str, yellows: str, grays: str) -> None:
+    def add_guess(self, guess: list[{chr, int}]) -> None:
         """Process the information coming from this new guess. This function also applies different
         heuristics/inferences I came up with to further narrow the information provided by each guess.
         Automatically updates the list of valid words.
         """
-        #TODO: Implement heuristics/inferences.
-        #TODO: Explain said heuristics/inferences in the README.
-        for k, l in enumerate(greens):
-            if l != '_':self._greens[k] = l
-        for k, l in enumerate(yellows):
-            if l != '_':
-                self._yellows[k].append(l)
-                self._all_yellows.add(l)
-        self._discarded = self._discarded | set(grays)
+        for i, t in enumerate(guess):
+            l = t['l']
+            col = t['c']
+            if col == green:
+                self._greens[i] = l
+            elif col == yellow:
+                self._yellows[i].append(l)
+                self._all_yellows.add(l) #I'm keeping a separate list of yellows so I don't have to flatten the self._yellows list
+            elif col == gray:
+                self._discarded = self._discarded | set(l)
+
         self._filter_words()
 
     def _filter_words(self) -> None:
@@ -58,15 +59,11 @@ class solver():
                 words_it_affects = words_it_affects | words_by_letter[letter]
         return len(words_it_affects)
 
-    def choose_word(self, any_word: bool = True) -> str:
+    def choose_word(self) -> str:
         """ 
         """
         #create a dictionary of the form {char: list[word]}, to see which letter maps to which of the remaining valid words.       
-        words_x_letter = self._e_d.words_by_letter(self.valid_words)       
-        if any_word:
-            words_to_choose_from = self._e_d.words_list
-        else:
-            words_to_choose_from = self.valid_words
-        best_word = max(words_to_choose_from, key=lambda x: self._how_many_discards(words_x_letter, x))
-        print(f"Affects {self._how_many_discards(words_x_letter, best_word)} words.")
-        return best_word
+        words_x_letter = self._e_d.words_by_letter(self.valid_words)
+        best_exploratory_word = max(self._e_d.words_list, key=lambda x: self._how_many_discards(words_x_letter, x))
+        best_valid_word = max(self.valid_words, key=lambda x: self._how_many_discards(words_x_letter, x))        
+        return best_exploratory_word, best_valid_word
